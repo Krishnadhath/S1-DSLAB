31e9c529d9e53ae64c0082907000c3d6cfdd9192
basilpoul <basilpaul230@gmail.com>
2025-08-21 14:55:37 +0530

    queueUsingArray v1 added

--- queueUsingArray.c
+++ queueUsingArray.c
@@ -6,10 +6,45 @@ int front = -1;
 int rear = -1;
 
 void enqueue(int value){
-    if(rear == 9)
-}
+    if(rear == 9){
+    printf("queue full");
+    }
+    if(front == -1){
+        front=0;
+        rear++;
+        queue[rear]=value;
+        printf("queued %d",value);
+    }
+
+    else{
+        rear++;
+        queue[rear]=value;
+        printf("queued %d",value);
+
+    }
+  }   
+    
+ 
+    void dequeue()
+    {
+        if (front==-1 || front>rear){
+            printf("queue is empty");
+        }
+        
+        printf("Dequed %d", queue[front]);
+        front++;
+    }
+    void display(){
+        
+
+    }
+
+
 
 
 int main(){
+    enqueue(29);
+    enqueue(20);
+    dequeue();
 
 }
