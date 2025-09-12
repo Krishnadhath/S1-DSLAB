fbe3b2b257c6e344ca60b8e8cb8ecf177f6ed1ec
basilpoul <basilpaul230@gmail.com>
2025-09-12 09:25:19 +0530

    stack using linkedlist created

--- mergeList.c
+++ mergeList.c
@@ -10,9 +10,10 @@ int main(){
 
     int num = 0;
     int list1 = 0;
-    printf("number of elements you need in first node");
+    printf("number of elements you in first node");
     scanf("%d",&num);
-    for(int i = 0; i< num; i++){
+    for(int i = 0; i< num; i++   ){
+
         
         printf("enter elements for the first list");
         scanf("%d",&list1);
